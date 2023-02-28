573aefea880e0477989eef1d60cbd650cbe4baea
Pedro Asevedo <pedrovictortec.ti@gmail.com>
2023-02-28 14:23:09 -0300

    commitando para salvar

--- src/contexts/FilmeDetails/FilmesDetailsProvider.tsx
+++ src/contexts/FilmeDetails/FilmesDetailsProvider.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { FilmeDetailsContext } from "./FilmeDetailsContex";
+import { Nota } from "../../dto/domain/Nota";
 import { Filme } from "../../dto/domain/Filme";
 import ProviderResult from "../../dto/contexts/providerResult";
 import { IFilmeDetailsProvider } from "../../dto/contexts/IFilmeDetailsProvider";
 import { filmDetailsService } from "../../services/requests/filmDetailsService";
+import { obterNotaService } from "../../utils/requests/obterNotaService";
 
 export function FilmeDetailsProvider(props) {
     const [filmeDetails, setFilmeDetails] = useState<Filme>([]);
+    const [nota, setNota] = useState<Nota>('');
     const [load, setLoad] = useState<boolean>(false);
-
 
     const FilmeDetailsValue: IFilmeDetailsProvider = {
         load,
         setLoad,
+        nota,
+        setNota,
         filmeDetails,
         setFilmeDetails,
         listarFilmeDetails: async (idFilme: number) => {
@@ -37,8 +41,30 @@ export function FilmeDetailsProvider(props) {
                 setLoad(false);
             }
             return ret
+        },
+        listarNotas: async (imdbId: string) => {
+            setLoad(true);
+            let ret: ProviderResult = null;
+            const requestResult = await obterNotaService(imdbId)
+            if (requestResult) {
+                console.log("Notas Provider Sucesso: ", requestResult);
+                setNota(requestResult);
+                ret = {
+                    ...ret,
+                    sucesso: true
+                },
+                    setLoad(false);
+            } else {
+                console.log("Notas Provider Falha: ", requestResult);
+                ret = {
+                    ...ret,
+                    sucesso: false,
+                    mensagemErro: requestResult
+                }
+                setLoad(false);
+            }
+            return ret
         }
-
     }
 
     return (
