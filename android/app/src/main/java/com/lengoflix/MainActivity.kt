bdd0ecbf13c9a692018b6a506c062595654ed1e6
Pedro Asevedo <pedrovictortec.ti@gmail.com>
2025-07-07 18:59:39 -0300

    instalando dependencias

--- android/app/src/main/java/com/lengoflix/MainActivity.kt
+++ android/app/src/main/java/com/lengoflix/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(null)
+  }
   override fun getMainComponentName(): String = "LengoFlix"
 
   /**
